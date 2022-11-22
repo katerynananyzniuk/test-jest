a479d6a4b95e23026072f78474ef2bf267f1effe
Kateryna Nanyzniuk <65312887+katerynananyzniuk@users.noreply.github.com>
2022-11-22 15:47:01 +0200

    feat: added jest mock for axios

--- swapiGetter.test.js
+++ swapiGetter.test.js
@@ -1,9 +1,22 @@
 import swapiGetter from './swapiGetter.js'
+import mockAxios from 'axios'
+
+jest.mock('axios', () => ({
+  __esModule: true,
+  default: {
+    get: jest.fn().mockResolvedValue({ data: { name: "Mock Jedi" } })
+  }
+}))
+//// another way to mock
+// jest.mock('axios')
+// mockAxios.get.mockResolvedValue({ data: { name: "Mock Jedi" } })
 
 describe('swapiGetter', () => {
+  afterEach(jest.clearAllMocks)
+
   test('should return a name', async () => {
     const result = await swapiGetter(1)
-    console.log(result)
-    expect(result).toBe('Luke Skywalker')
+    expect(result).toBe('Mock Jedi')
+    expect(mockAxios.get).toHaveBeenCalledTimes(1)
   })
 })
